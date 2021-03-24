8a3e005d9a3b946e5c70e890e968b66fc2803233
Manik <manikchhabra10@gmail.com>
2021-03-24 21:30:32 +0530

    "ui changes"

--- src/components/Body/CampaignItem/CampaignFrame/CampaignFrame.js
+++ src/components/Body/CampaignItem/CampaignFrame/CampaignFrame.js
@@ -2,6 +2,10 @@ import CampaignItem from '../CampaignItem';
 import './CampaignFrame.css';
 
 const CampaignFrame=({data})=>{
+
+    const onActiveClick=()=>{
+        console.log("clicked");
+    }
 return(
     <>
     <div className="frameDetail grid-container" >
@@ -11,7 +15,7 @@ return(
         <div>ACTIONS</div>
     </div>
     {
-        data.map(detail=><CampaignItem detail={detail} key={detail.region}/>)
+        data.map(detail=> <CampaignItem detail={detail} key={detail.region} onClick={onActiveClick} />)
     }
     
  </>
